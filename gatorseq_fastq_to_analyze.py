26522967d623e81c0c7163aa6001e5f03d375c04
Majety <s.majety@ufl.edu>
2019-12-23 15:03:11 -0500

    added lock file

--- gatorseq_fastq_to_analyze.py
+++ gatorseq_fastq_to_analyze.py
@@ -7,6 +7,7 @@ import subprocess
 import shlex
 import yaml
 import pandas as pd
+from filelock import FileLock
 
 print(str(datetime.datetime.now()) + "\n")
 
@@ -121,201 +122,214 @@ def populate_error_msg(row, error_message, xldf):
 # This checks if workbook is open; if so it will exit out
 
 if __name__ == "__main__":
-    
-    try: 
-        excel_file = open(GATOR_SEQ_SAMPLE_INPUT_FILE, "r+")
+
+    file_to_lock =  GATOR_SEQ_SAMPLE_INPUT_FILE + ".lock"
+    lock = FileLock(file_to_lock)
+    try:
+        lock.acquire(timeout=1)
     except:
-        print(" Could not open file! Please close Excel!")
+        print("some other script is using it")
         sys.exit()
 
     try:
-            xldf_full = pd.read_excel(GATOR_SEQ_SAMPLE_INPUT_FILE)
-            xldf = xldf_full.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-            xldf = xldf.astype('object')
-            excel_file.close()
-    except:
-        print("Problem Reading Excel")
-        sys.exit()
-    
 
+        try: 
+            excel_file = open(GATOR_SEQ_SAMPLE_INPUT_FILE, "r+")
+        except:
+            print(" Could not open file! Please close Excel!")
+            sys.exit()
 
-    for index, row in xldf.iterrows():
-        
-        #print(row)
-        sample_path=xldf.at[index, 'SAMPLE_DIR_PATH']
-        status=xldf.at[index, "STATUS"]
-        #print(sample_path)
-        #print(status)
-        #print(list(status))
-
-        ## Check if the sample directory exist
-        # Strips from both sites for following characaters ' ', '\t', '\r', '\/', '.', '*', '+'
         try:
-            sample_path = sample_path.strip(' \t\n\r\/.\*\+')
-        except Exception:
-            populate_error_msg(row, "ERROR: Empty directory", xldf)
-            continue
-
-        if sample_path == '':
-            xldf.at[index, "STATUS"] = "FAILED"
-            xldf.at[index, "MESSAGE"] = "ERROR: Invalid directory"
-            save_workbook(xldf)
-            continue
-
-        run_prefix = sample_path.split('/')[0]
-        sample_prefix = sample_path.split('/')[1]
+                xldf_full = pd.read_excel(GATOR_SEQ_SAMPLE_INPUT_FILE)
+                xldf = xldf_full.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+                xldf = xldf.astype('object')
+                
+        except:
+            print("Problem Reading Excel")
+            sys.exit()
         
-        sample_path = LINUX_PATHOLOGY_FASTQ_FOLDER + "/" + run_prefix + "-*/" +sample_prefix + "-*"
-        listing = glob.glob(sample_path)
-        directories=[d for d in listing if os.path.isdir(d)]
-        run_name = ''
-        sample_name = ''
-
-        if len(directories) == 0:
-            #ToDO: replace error messages with definition
-            xldf.at[index, "STATUS"] = "FAILED"
-            xldf.at[index, "MESSAGE"] = "ERROR: Following directory extention not found:" + sample_path
-            save_workbook(xldf)
-            continue
-        elif len(directories) > 1:
-            xldf.at[index, "STATUS"] = "FAILED"
-            xldf.at[index, "MESSAGE"] = "ERROR: More than one directory extention found:" + ';'.join(directories)
-            save_workbook(xldf)
-            continue
-        else:
-            sample_path = directories[0]
-            #run_name = sample_path.split('/')[-2]
-            #sample_name = sample_path.split('/')[-1]
-
-        linux_hpc_sample_path =  LINUX_HPC_FASTQ_FOLDER + "/" + run_prefix + "-*/" +sample_prefix + "-*"
-        hpc_sample_path = ""
-        listing = glob.glob(linux_hpc_sample_path)
-        directories=[d for d in listing if os.path.isdir(d)]
-        if len(directories) == 0:
-            xldf.at[index, "STATUS"] = "RUN"
-            xldf.at[index, "MESSAGE"] = "WARNING: Directory is not yet uploaded to HPC:" + linux_hpc_sample_path
-            save_workbook(xldf)
-            continue
-        elif len(directories) > 1:
-            xldf.at[index, "STATUS"] = "FAILED"
-            xldf.at[index, "MESSAGE"] = "ERROR: More than one directory extention found on HPC:" + ';'.join(directories)
-            save_workbook(xldf)
-            continue
-        else:
-            linux_hpc_sample_path = directories[0]
-            sample_name = sample_path.split('/')[-1]
-            run_name = sample_path.split('/')[-2]
-            hpc_sample_path =  HPC_FASTQ_FOLDER + "/" + run_name + "/" +sample_name 
 
 
+        for index, row in xldf.iterrows():
+            
+            #print(row)
+            sample_path=xldf.at[index, 'SAMPLE_DIR_PATH']
+            status=xldf.at[index, "STATUS"]
+            #print(sample_path)
+            #print(status)
+            #print(list(status))
 
-
-        ## Check if the sample directory exist
-        if status == 'RUN':
-            ### Check sample folders are synced properly 
-            rsync_check_cmd =  'rsync -ltgoDvzrn --progress --stats ' +\
-                sample_path + "/ " +\
-                linux_hpc_sample_path + '/ |grep "Total transferred file size: 0 bytes"'
-            proc = subprocess.Popen(rsync_check_cmd, shell=True, stdout=subprocess.PIPE)
-            #need to decode from bytes to str
-            stdout_list = proc.communicate()[0].decode()
-            if len(stdout_list) == 0:
-                xldf.at[index, "STATUS"] = "RUN"
-                xldf.at[index, "MESSAGE"] = "WARNING: Directory is not completely in sync with HPC folder:" + linux_hpc_sample_path
-                save_workbook(xldf)
-
-                rsync_hpc_check_cmd =  'rsync -ltgoDvzrn --progress --stats ' +\
-                    sample_path + "/ " + HPC_SFTP + ":" +\
-                    hpc_sample_path + '/ |grep "Total transferred file size: 0 bytes"'
-                proc = subprocess.Popen(rsync_hpc_check_cmd, shell=True, stdout=subprocess.PIPE)
-                #need to decode from bytes to str
-                stdout_list = proc.communicate()[0].decode()
-                if len(stdout_list) != 0:
-                    umount_cmd = '/sbin/umount '+ LOCAL_HPC_MNT + ';'
-                    proc = subprocess.Popen(umount_cmd, shell=True, stdout=subprocess.PIPE)
-                    sys.exit("ERROR: mount snyc problem; executed "+ umount_cmd + "\n")
-
+            ## Check if the sample directory exist
+            # Strips from both sites for following characaters ' ', '\t', '\r', '\/', '.', '*', '+'
+            try:
+                sample_path = sample_path.strip(' \t\n\r\/.\*\+')
+            except Exception:
+                populate_error_msg(row, "ERROR: Empty directory", xldf)
                 continue
 
-            time_stamp = str(datetime.datetime.now()).replace('-', '').replace(' ','').replace(':', '').replace('.', '')
-            time_stamp= time_stamp +CODE_ENV+GSBW_VERSION
+            if sample_path == '':
+                xldf.at[index, "STATUS"] = "FAILED"
+                xldf.at[index, "MESSAGE"] = "ERROR: Invalid directory"
+                save_workbook(xldf)
+                continue
 
-            run_log = sample_path + "/" + time_stamp + ".log"
+            run_prefix = sample_path.split('/')[0]
+            sample_prefix = sample_path.split('/')[1]
+            
+            sample_path = LINUX_PATHOLOGY_FASTQ_FOLDER + "/" + run_prefix + "-*/" +sample_prefix + "-*"
+            listing = glob.glob(sample_path)
+            directories=[d for d in listing if os.path.isdir(d)]
+            run_name = ''
+            sample_name = ''
 
-            xldf.at[index, 'STATUS'] = "SUBMITTED"
-            xldf.at[index, 'TIME_STAMP'] = time_stamp 
-            xldf.at[index, 'MESSAGE'] = "Currently job is running."
-            save_workbook(xldf)
+            if len(directories) == 0:
+                #ToDO: replace error messages with definition
+                xldf.at[index, "STATUS"] = "FAILED"
+                xldf.at[index, "MESSAGE"] = "ERROR: Following directory extention not found:" + sample_path
+                save_workbook(xldf)
+                continue
+            elif len(directories) > 1:
+                xldf.at[index, "STATUS"] = "FAILED"
+                xldf.at[index, "MESSAGE"] = "ERROR: More than one directory extention found:" + ';'.join(directories)
+                save_workbook(xldf)
+                continue
+            else:
+                sample_path = directories[0]
+                #run_name = sample_path.split('/')[-2]
+                #sample_name = sample_path.split('/')[-1]
 
-            HPC_JOB_PREFIX= sample_prefix+ "_"+ time_stamp
-            HPC_RUN_DIR=HPC_ANALYSIS_FOLDER+"/"+ HPC_JOB_PREFIX
-            LINUX_HPC_RUN_DIR_CRONJOB=LINUX_HPC_ANALYSIS_FOLDER+"/"+ HPC_JOB_PREFIX + ".cronjob.sh"
-            LINUX_HPC_RUN_DIR_CRONJOB_LOG = LINUX_HPC_ANALYSIS_FOLDER + "/" + HPC_JOB_PREFIX + ".cronjon.sh.log"
-            #FASTQ_FILES_DIR=HPC_FASTQ_FOLDER+'/'+run_prefix+'*/'+sample_prefix+'*'
-            FASTQ_ROOT_DIR_SUFFIX=run_prefix+'*/'+sample_prefix+'*'
-            #print(FASTQ_FILES_DIR)
-
-            HPC_RUN_CMD =   "#!/bin/bash\n" 
-
-            HPC_RUN_CMD = HPC_RUN_CMD + "SAMPLE_RUN_DIR=" + HPC_RUN_DIR +  ";\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f $SAMPLE_RUN_DIR" +  ";\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "mkdir $SAMPLE_RUN_DIR" + ";\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "chmod -R 750 $SAMPLE_RUN_DIR" + ";\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "cd $SAMPLE_RUN_DIR" ";\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "\n\n"
-
-
-            HPC_RUN_CMD = HPC_RUN_CMD + "export NXF_TEMP=$SAMPLE_RUN_DIR"+"\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "export NXF_ASSETS=$SAMPLE_RUN_DIR"+"\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + HPC_NEXTFLOW_PROGRAM + " run \\\n" +\
-            GATOR_SEQ_HPC_CODE_DIR +" -r " + GSBW_VERSION + " \\\n"+\
-            "  -with-report -with-trace -with-timeline -with-dag flowchart.html \\\n" +\
-            "  -resume \\\n" +\
-            " --FASTQ_ROOT_DIR_SUFFIX=" + FASTQ_ROOT_DIR_SUFFIX + " \\\n" +\
-            " --SAMPLE_DIR=$SAMPLE_RUN_DIR" + " \\\n" +\
-            " --RUN_NAME=" + run_prefix + " \\\n" +\
-            " --SAMPLE_NAME=" + sample_prefix + " \\\n" +\
-            " --TIME_STAMP=" + time_stamp + " \\\n" +\
-            " --CODE_ENV=" + CODE_ENV + " ;\n"
+            linux_hpc_sample_path =  LINUX_HPC_FASTQ_FOLDER + "/" + run_prefix + "-*/" +sample_prefix + "-*"
+            hpc_sample_path = ""
+            listing = glob.glob(linux_hpc_sample_path)
+            directories=[d for d in listing if os.path.isdir(d)]
+            if len(directories) == 0:
+                xldf.at[index, "STATUS"] = "RUN"
+                xldf.at[index, "MESSAGE"] = "WARNING: Directory is not yet uploaded to HPC:" + linux_hpc_sample_path
+                save_workbook(xldf)
+                continue
+            elif len(directories) > 1:
+                xldf.at[index, "STATUS"] = "FAILED"
+                xldf.at[index, "MESSAGE"] = "ERROR: More than one directory extention found on HPC:" + ';'.join(directories)
+                save_workbook(xldf)
+                continue
+            else:
+                linux_hpc_sample_path = directories[0]
+                sample_name = sample_path.split('/')[-1]
+                run_name = sample_path.split('/')[-2]
+                hpc_sample_path =  HPC_FASTQ_FOLDER + "/" + run_name + "/" +sample_name 
 
 
 
-            HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/.nextflow* ;\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/work ;\n"
-            HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/" + NEXTFLOW_GIT_REPO + ";\n"
 
-            cron_fw = open(LINUX_HPC_RUN_DIR_CRONJOB,'w')
-            cron_fw.write(HPC_RUN_CMD)
-            cron_fw.close()
-            #print(HPC_RUN_CMD)
-            print("Submitted: "+ LINUX_HPC_RUN_DIR_CRONJOB + "\n")
-
-        elif status == 'SUBMITTED':
-            time_stamp = xldf.at[index, "TIME_STAMP"]
-            linux_analysis_out_sample_dir = LINUX_ANALYSIS_OUT_FOLDER + "/" + run_prefix + "/" + sample_prefix + "_" + time_stamp
-            hpc_analysis_out_sample_dir = LINUX_HPC_ANALYSIS_FOLDER + "/" + run_prefix + "/" + sample_prefix + "_" + time_stamp
-
-            #run_log = sample_path + "/" + time_stamp + ".log"
-            if os.path.isdir(linux_analysis_out_sample_dir):
-                if os.path.isdir(hpc_analysis_out_sample_dir):
-                    pass
-                elif os.path.isfile(linux_analysis_out_sample_dir + "/SUCCESS.txt"):
-                    xldf.at[index, "STATUS"] = "DONE"
-                    xldf.at[index, "MESSAGE"] = "Successfully processed"
-                    save_workbook(xldf)
-                elif os.path.isfile(linux_analysis_out_sample_dir + "/FAILED.txt"):
-                    xldf.at[index, "STATUS"] = "FAILED"
-                    xldf.at[index, "MESSAGE"] = "ERROR: Failed while running on HPC"
+            ## Check if the sample directory exist
+            if status == 'RUN':
+                ### Check sample folders are synced properly 
+                rsync_check_cmd =  'rsync -ltgoDvzrn --progress --stats ' +\
+                    sample_path + "/ " +\
+                    linux_hpc_sample_path + '/ |grep "Total transferred file size: 0 bytes"'
+                proc = subprocess.Popen(rsync_check_cmd, shell=True, stdout=subprocess.PIPE)
+                #need to decode from bytes to str
+                stdout_list = proc.communicate()[0].decode()
+                if len(stdout_list) == 0:
+                    xldf.at[index, "STATUS"] = "RUN"
+                    xldf.at[index, "MESSAGE"] = "WARNING: Directory is not completely in sync with HPC folder:" + linux_hpc_sample_path
                     save_workbook(xldf)
 
-        elif status == 'FAILED':
-            pass
+                    rsync_hpc_check_cmd =  'rsync -ltgoDvzrn --progress --stats ' +\
+                        sample_path + "/ " + HPC_SFTP + ":" +\
+                        hpc_sample_path + '/ |grep "Total transferred file size: 0 bytes"'
+                    proc = subprocess.Popen(rsync_hpc_check_cmd, shell=True, stdout=subprocess.PIPE)
+                    #need to decode from bytes to str
+                    stdout_list = proc.communicate()[0].decode()
+                    if len(stdout_list) != 0:
+                        umount_cmd = '/sbin/umount '+ LOCAL_HPC_MNT + ';'
+                        proc = subprocess.Popen(umount_cmd, shell=True, stdout=subprocess.PIPE)
+                        sys.exit("ERROR: mount snyc problem; executed "+ umount_cmd + "\n")
 
-        elif status == 'DONE':
-            pass
+                    continue
 
-        else:
-            xldf.at[index, "MESSAGE"] = "ERROR: Invalid Status." 
-            save_workbook(xldf)
+                time_stamp = str(datetime.datetime.now()).replace('-', '').replace(' ','').replace(':', '').replace('.', '')
+                time_stamp= time_stamp +CODE_ENV+GSBW_VERSION
+
+                run_log = sample_path + "/" + time_stamp + ".log"
+
+                xldf.at[index, 'STATUS'] = "SUBMITTED"
+                xldf.at[index, 'TIME_STAMP'] = time_stamp 
+                xldf.at[index, 'MESSAGE'] = "Currently job is running."
+                save_workbook(xldf)
+
+                HPC_JOB_PREFIX= sample_prefix+ "_"+ time_stamp
+                HPC_RUN_DIR=HPC_ANALYSIS_FOLDER+"/"+ HPC_JOB_PREFIX
+                LINUX_HPC_RUN_DIR_CRONJOB=LINUX_HPC_ANALYSIS_FOLDER+"/"+ HPC_JOB_PREFIX + ".cronjob.sh"
+                LINUX_HPC_RUN_DIR_CRONJOB_LOG = LINUX_HPC_ANALYSIS_FOLDER + "/" + HPC_JOB_PREFIX + ".cronjon.sh.log"
+                #FASTQ_FILES_DIR=HPC_FASTQ_FOLDER+'/'+run_prefix+'*/'+sample_prefix+'*'
+                FASTQ_ROOT_DIR_SUFFIX=run_prefix+'*/'+sample_prefix+'*'
+                #print(FASTQ_FILES_DIR)
+
+                HPC_RUN_CMD =   "#!/bin/bash\n" 
+
+                HPC_RUN_CMD = HPC_RUN_CMD + "SAMPLE_RUN_DIR=" + HPC_RUN_DIR +  ";\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f $SAMPLE_RUN_DIR" +  ";\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "mkdir $SAMPLE_RUN_DIR" + ";\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "chmod -R 750 $SAMPLE_RUN_DIR" + ";\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "cd $SAMPLE_RUN_DIR" ";\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "\n\n"
+
+
+                HPC_RUN_CMD = HPC_RUN_CMD + "export NXF_TEMP=$SAMPLE_RUN_DIR"+"\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "export NXF_ASSETS=$SAMPLE_RUN_DIR"+"\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + HPC_NEXTFLOW_PROGRAM + " run \\\n" +\
+                GATOR_SEQ_HPC_CODE_DIR +" -r " + GSBW_VERSION + " \\\n"+\
+                "  -with-report -with-trace -with-timeline -with-dag flowchart.html \\\n" +\
+                "  -resume \\\n" +\
+                " --FASTQ_ROOT_DIR_SUFFIX=" + FASTQ_ROOT_DIR_SUFFIX + " \\\n" +\
+                " --SAMPLE_DIR=$SAMPLE_RUN_DIR" + " \\\n" +\
+                " --RUN_NAME=" + run_prefix + " \\\n" +\
+                " --SAMPLE_NAME=" + sample_prefix + " \\\n" +\
+                " --TIME_STAMP=" + time_stamp + " \\\n" +\
+                " --CODE_ENV=" + CODE_ENV + " ;\n"
+
+
+
+                HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/.nextflow* ;\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/work ;\n"
+                HPC_RUN_CMD = HPC_RUN_CMD + "rm -r -f " + HPC_RUN_DIR + "/" + NEXTFLOW_GIT_REPO + ";\n"
+
+                cron_fw = open(LINUX_HPC_RUN_DIR_CRONJOB,'w')
+                cron_fw.write(HPC_RUN_CMD)
+                cron_fw.close()
+                #print(HPC_RUN_CMD)
+                print("Submitted: "+ LINUX_HPC_RUN_DIR_CRONJOB + "\n")
+
+            elif status == 'SUBMITTED':
+                time_stamp = xldf.at[index, "TIME_STAMP"]
+                linux_analysis_out_sample_dir = LINUX_ANALYSIS_OUT_FOLDER + "/" + run_prefix + "/" + sample_prefix + "_" + time_stamp
+                hpc_analysis_out_sample_dir = LINUX_HPC_ANALYSIS_FOLDER + "/" + run_prefix + "/" + sample_prefix + "_" + time_stamp
+
+                #run_log = sample_path + "/" + time_stamp + ".log"
+                if os.path.isdir(linux_analysis_out_sample_dir):
+                    if os.path.isdir(hpc_analysis_out_sample_dir):
+                        pass
+                    elif os.path.isfile(linux_analysis_out_sample_dir + "/SUCCESS.txt"):
+                        xldf.at[index, "STATUS"] = "DONE"
+                        xldf.at[index, "MESSAGE"] = "Successfully processed"
+                        save_workbook(xldf)
+                    elif os.path.isfile(linux_analysis_out_sample_dir + "/FAILED.txt"):
+                        xldf.at[index, "STATUS"] = "FAILED"
+                        xldf.at[index, "MESSAGE"] = "ERROR: Failed while running on HPC"
+                        save_workbook(xldf)
+
+            elif status == 'FAILED':
+                pass
+
+            elif status == 'DONE':
+                pass
+
+            else:
+                xldf.at[index, "MESSAGE"] = "ERROR: Invalid Status." 
+                save_workbook(xldf)
         
+        excel_file.close()
+    finally:
+        lock.release()
 
