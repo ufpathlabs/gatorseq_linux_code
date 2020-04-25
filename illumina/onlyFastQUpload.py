a0775fb514e5c68e2af1d93d630dc59b067fe006
Siddardha Majety <s.majety@ahc-path-data19.ahc.ufl.edu>
2020-04-25 09:55:27 -0400

    added missing paramaeter

--- illumina/onlyFastQUpload.py
+++ illumina/onlyFastQUpload.py
@@ -115,12 +115,13 @@ def uploadFastQ(conn, baseMountDir):
     cur.execute("SELECT * FROM "+TRUSIGHT_TABLE_NAME+" where STATUS = 'UPLOAD_FASTQ_PENDING';")
     rows = cur.fetchall()
     cur.close()
-    rows = rows[:35]
+    rows = rows[:5]
     pool = mp.Pool(processes=len(rows))
 
-    results = [pool.apply(processCode, args=(row[0], row[4],)) for row in rows]
+    results = [pool.apply_async(processCode, args=(row[0], row[4],conn,)) for row in rows]
     
-    print(results)
+    output = [p.get() for p in results]
+    print(output)
     # pool.apply(processCode, args=())
     # for row in rows:
     #     sampleName = row[0]
