bbcbd68be7156fe1db330ac71446a5e5ca3d2f7a
riyaz.shaik <riyaz.kvgb@gmail.com>
2021-12-13 08:09:19 -0500

    updates filename logic to relax .s

--- NGS21/EPIC_updates/hl7conv/hl7conv/common.py
+++ NGS21/EPIC_updates/hl7conv/hl7conv/common.py
@@ -225,7 +225,7 @@ def get_as_af(record, ratio_ad_dp, xml):
 
 def validate_filename(filename):
     print(filename)
-    pattern = '^[\w,\s\-,/]+\.(xml|vcf|vcf\.gz)$'
+    pattern = '^[\w,\s\-,/,\.]+\.(xml|vcf|vcf\.gz)$'
     result = re.match(pattern, filename)
     return bool(result)
 
